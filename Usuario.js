9bd0ecf33097ff0725352c01624e4fad51bc1ffd
Lucas Bello <bellolucas@hotmail.com>
2023-04-11 22:26:01 -0300

    pagina index

--- Usuario.js
+++ Usuario.js
@@ -1,10 +1,22 @@
-function Usuario (nombre,contrasena){
-    //propiedad =parametro
-    this.nombre = nombre;
-    this.contrasena = contrasena;
+class Usuario1 {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    mostrar() {
+        alert("Hola " + "" + this.nombre)
+    }
 }
-const usuario1 = new Usuario("Lucas","777")
+
+
+let usuario1 = new Usuario1(
+    prompt("Ingrese nombre"),
+    prompt("Ingrese edad")
+);
+console.log(usuario1.mostrar());
+
+/*const usuario1 = new Usuario("Lucas","777")
 console.log(usuario1)
 
 const usuario2 = new Usuario("Homero", "888")
-console.log(usuario2)
+console.log(usuario2)*/
